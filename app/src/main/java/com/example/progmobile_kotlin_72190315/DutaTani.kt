4f1728549bcb96279fa1d6f7b1c63b0ed6756f4a
aliung ferry <sdaliungferry@gmail.com>
2021-09-14 17:14:15 +0700

    Terlambat Membuat DUTATANI

---
+++ app/src/main/java/com/example/progmobile_kotlin_72190315/DutaTani.kt
@@ -0,0 +1,20 @@
+package com.example.progmobile_kotlin_72190315
+
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import com.google.android.material.snackbar.Snackbar
+import org.w3c.dom.Text
+
+class DutaTani : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_duta_tani)
+    }
+}
